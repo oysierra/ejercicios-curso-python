d3e93fac6eb5adafe7d4552c2f7acd6aa7becc1f
Omar Sierra <oysierra@gmail.com>
2022-11-20 22:46:05 -0500

    Main

---
+++ Parte 1/DebanadodeCadena.py
@@ -0,0 +1,8 @@
+cadena = "Esto es una cadena (no importa el tamaño), es debabado o substring"
+
+print(len(cadena)) #el len devuelve el tamaño de la cadena estilo length
+
+print(cadena[2]) #imprime que letra o numero hay en la posición de la cadena
+
+print(cadena[0:9]) # El debanado de la cadena es mostrar o recorrer la cadena desde un punto hasta otro
+
