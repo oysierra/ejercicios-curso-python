38a4b410b88640cec66588154bd0bd922543ce24
Omar Sierra <oysierra@gmail.com>
2022-11-20 22:49:17 -0500

    Nueva Actualización Otros cambio del curso de python

--- Parte 1/DebanadodeCadena.py
+++ Parte 1/DebanadodeCadena.py
@@ -6,3 +6,4 @@ print(cadena[2]) #imprime que letra o numero hay en la posición de la cadena
 
 print(cadena[0:9]) # El debanado de la cadena es mostrar o recorrer la cadena desde un punto hasta otro
 
+print("he hecho un pull")
