f8a0688618669eb18f0864116c7b15c5ae8be4a4
Omar Sierra <oysierra@gmail.com>
2022-11-26 21:34:34 -0500

    Modificación de cadena

--- Parte 1/MetodoString.py
+++ Parte 1/MetodoString.py
@@ -1,4 +1,7 @@
-cadena = "Estoy utilizando los metodos de python"
+cadena = "EstOY utiLizaNdo lOS mEtoDoS dE pYtHoN"
 
 print(cadena.lower()) # Pone la cadena en minuscula
-print(cadena.upper) # Pone la cadena en mayuscula
+print(cadena.upper()) # Pone la cadena en mayuscula
+print(cadena.capitalize()) # Pone la primera mayuscula en la cadena
+print(cadena.title()) # Pone la mayuscula en cada palabra
+print(cadena.swapcase()) # invierte mayusculas en minusculas y viceversa
