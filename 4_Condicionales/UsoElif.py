7b38973be60e564799d17a31ac6819cb48f73e99
Omar Sierra <oysierra@gmail.com>
2022-11-29 10:38:37 -0500

    Se agrega la carpeta condicionales

---
+++ 4_Condicionales/UsoElif.py
@@ -0,0 +1,13 @@
+# elif evalua de forma consecutiva los if anteriores
+letra = "U"
+
+if letra.lower() == "a":
+    print("Esta vocal es la A")
+elif letra.lower() == "e":
+    print("Esta vocal es la E")
+elif letra.lower() == "i":
+    print("Esta vocal es la I")
+elif letra.lower() == "o":
+    print("Esta vocal es la O")
+else:
+    print("Esta vocal es la U")
---
+++ 4_Condicionales/UsoIfElse.py
@@ -0,0 +1,8 @@
+# La indentación es importante para las condicionales, de lo contrario se presenta un error
+
+edad = 20
+
+if edad >= 18:
+    print("Tu eres mayor de edad SI puedes tener cedula")
+else:
+    print("Tu NO eres mayor de edad NO puedes tener cedula")
