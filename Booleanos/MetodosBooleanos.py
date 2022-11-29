0dd30d4f0c6767944458a65fdb80f3fb36ae0c29
Omar Sierra <oysierra@gmail.com>
2022-11-29 10:14:23 -0500

    Se agregan los metodos booleanos

---
+++ Booleanos/MetodosBooleanos.py
@@ -0,0 +1,15 @@
+cadena = "Estoy mostrando los metodos booleanos para las cadenas string"
+cadena_2 = "HOLA LINA"
+cadena_3 = "hola omar"
+
+# Verifica si la cadena comienza con el parametro que se le está indicando
+print(cadena.startswith("E"))
+
+# Verifica si la cadena termina con el parametro que se le está indicando
+print(cadena.endswith("s"))
+
+# Verifica si la toda la cadena esta en mayuscula
+print(cadena_2.isupper())
+
+# Verifica si la toda la cadena esta en minuscula
+print(cadena_3.islower())
