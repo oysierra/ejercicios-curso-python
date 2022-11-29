116c2e71a800de94733b25621f5dbeb717b7e36c
Omar Sierra <oysierra@gmail.com>
2022-11-29 10:45:09 -0500

    Se agrega Condicionales anidados

---
+++ 4_Condicionales/CondicionalesAnidados.py
@@ -0,0 +1,10 @@
+nombre = "Lina"
+edad = 17
+
+if nombre == "Lina":
+    if edad >= 18:
+        print("Tu eres Lina y eres MAYOR de edad")
+    else:
+        print("Eres Lina pero eres MENOR de edad")
+else:
+    print("Tu no eres Lina")
