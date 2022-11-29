791d0baa55b21e22a76081e2cf43b436515039d9
Omar Sierra <oysierra@gmail.com>
2022-11-29 09:04:41 -0500

    Correccion_ejercicio2_entrada_teclado

--- entrada_por_teclado_salida_en_pantalla/Ejercicio2_Entrada_Teclado.py
+++ entrada_por_teclado_salida_en_pantalla/Ejercicio2_Entrada_Teclado.py
@@ -12,5 +12,5 @@ promedio_prueba = (prueba_1 + prueba_2 + prueba_3) / 3
 
 promedio_final = (promedio_prueba + (2 * examen_parcial) + (3 * examen_final))/6
 
-print(promedio_final)
+print("El promedio de la practica es: ", promedio_prueba , " y el promedio final es: ", promedio_final)
 
