00a02464596112bdff2c35c818d929cc25408f5d
Omar Sierra <oysierra@gmail.com>
2022-12-01 20:04:38 -0500

    Se agregan los ejercicios 1 y 2

--- 5_Estructura_de_Datos_Listas/Ejercicio1EstructuradeDatos.py
+++ 5_Estructura_de_Datos_Listas/Ejercicio1EstructuradeDatos.py
@@ -1,2 +1,15 @@
 # En la siguiente lista, debes hacer un programa que muestre los valores al usuario, 
 # a su vez, debe pedir dos datos y esos que sean ingresados deben ser sustituidos en el primer y segundo lugar:[20, 50, "Curso", 'Python', 3.14]
+
+lista = [20, 50, "Curso", 'Python', 3.14]
+print(lista)
+
+elemento = input("Digite un elemento númerico, letra o palabra: ")
+
+lista[0] = elemento
+
+elemento = input("Digite otro elemento númerico, letra o palabra: ")
+
+lista[1] = elemento
+
+print(lista)
