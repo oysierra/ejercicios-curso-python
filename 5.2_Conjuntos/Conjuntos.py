81587f191a67a0eb3985d8da73ef0a7ece18a559
Omar Sierra <oysierra@gmail.com>
2022-12-04 11:29:50 -0500

    Se agrega la carpeta conjuntos

---
+++ 5.2_Conjuntos/Conjuntos.py
@@ -0,0 +1,7 @@
+# Los conjunto se caracterizan por ser de 1 solo tipo de dato, no permite datos repetidos
+# El conjunto se identifica como tipo de dato "set", normalmente inicializa con {}
+# Otra forma de inicializarlo es: set[(datos)] o set((datos)), puede aparecer como type GenericAlias
+
+conjunto = {1 , 2 , 3 , 4 , 5}
+
+print(type(conjunto))
