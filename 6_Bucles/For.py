9d8ca8021f0201497e49eee72898c8e685226278
Omar Sierra <oysierra@gmail.com>
2022-12-08 17:18:41 -0500

    Se agregar el for y el range

---
+++ 6_Bucles/For.py
@@ -0,0 +1,10 @@
+# El objetivo del For es recorrer elemento iterable (listas)
+
+lista = ["uno" , "dos" , "tres"]
+tupla = (1 , 2 , 3 , 4 , 5)
+
+for i in lista:
+    print(i)
+
+for j in tupla:
+    print(j)
---
+++ 6_Bucles/Range.py
@@ -0,0 +1,6 @@
+
+for i in range(1 , 10 , 3): # el primer valor es donde inicia, el segundo donde termina y si se le agrega un tercero será de cuanto en cuanto irá aumentando
+    print(i)
+
+for j in range(-10,-1):
+    print(j)
