82d404cbe6bacddad19eda7aaf0b2f9f417f3c45
Omar Sierra <oysierra@gmail.com>
2023-01-04 21:07:46 -0500

    Se crea funcion propia y creada

---
+++ 7_Funciones/Funciones_Creadas.py
@@ -0,0 +1,15 @@
+# Creamos funciones propias
+
+'''def <nombre de la funcion>():
+    <sentencias> ''' # es importante la indentacion
+
+def saludo():
+    print("Hola a mi primer funcion")
+
+saludo()
+
+def tabla7():
+    for i in range(11):
+        print("7 x {} = {}".format(i , i*7))
+
+tabla7()
