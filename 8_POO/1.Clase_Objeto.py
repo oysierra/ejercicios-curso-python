6c393bf32f4b1a6a8389f48dc627493985ae78cb
Omar Sierra <oysierra@gmail.com>
2023-01-10 21:18:47 -0500

    Se agrega lña carpeta POO

---
+++ 8_POO/1.Clase_Objeto.py
@@ -0,0 +1,13 @@
+'''class <nombre de la clase>():
+    <sentencias>
+    pass'''
+
+class FabricaTelefonos(): # se recomienda usar la mayuscula inicial para identificarla como clase
+    pass
+
+print(type(FabricaTelefonos))
+
+celular = FabricaTelefonos() # Instruccion para crear un objeto de esa clase
+celular2 = FabricaTelefonos()
+
+print(type(celular))
