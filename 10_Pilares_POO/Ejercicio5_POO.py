ef5fa4ac56cb07ae9f80f4c291b01e70be4119bd
Omar Sierra <oysierra@gmail.com>
2023-02-01 23:37:34 -0500

    Se crea el ejercicio 5 sin resolver aún

---
+++ 10_Pilares_POO/Ejercicio5_POO.py
@@ -0,0 +1,5 @@
+# Crear un programa con tres clases:
+# Universidad, con atributos nombre (Donde se almacena el nombre de la Universidad). 
+# Otra llamada Carerra, con los atributos especialidad (En donde me guarda la especialidad de un estudiante). 
+# Una ultima llamada Estudiante, que tenga como atributos su nombre y edad. 
+# El programa debe imprimir la especialidad, edad, nombre y universidad de dicho estudiante con un objeto llamado persona.
